e903ce1c57749bceb66b35855a3b2cff267c6e99
Alex Mayfield <alexmax2742@gmail.com>
2014-06-24 20:44:08 -0400

    Fully promisify database tests.

--- test/test_dbconn.js
+++ test/test_dbconn.js
@@ -10,24 +10,18 @@ var error = require('../error');
 
 describe('DBConn', function() {
 	describe('new DBConn()', function() {
-		it("should construct correctly.", function(done) {
-			new DBConn({
+		it("should construct correctly.", function() {
+			return new DBConn({
 				database: {
 					uri: "sqlite://charonauth/",
 					options: { "storage": ":memory:" }
-				}
-			}).then(function(db) {
-				if (!(db instanceof DBConn)) {
-					done(new Error("Did not get a DBConn from constructor"));
-				} else {
-					done();
 				}
 			});
 		});
 	});
 	describe('DBConn.addUser()', function() {
-		it("should correctly add a user.", function(done) {
-			new DBConn({
+		it("should correctly add a user.", function() {
+			return new DBConn({
 				database: {
 					uri: "sqlite://charonauth/",
 					options: { "storage": ":memory:" }
@@ -35,15 +29,11 @@ describe('DBConn', function() {
 			}).then(function(dbconn) {
 				return dbconn.addUser('username', 'password123', 'example@example.com')
 			}).then(function(user) {
-				if (user.username !== 'username') {
-					done(new Error("User was not added as expected"));
-				} else {
-					done();
-				}
+				assert.equal(user.username, 'username');
 			});
 		});
-		it("should correctly lowercase a username.", function(done) {
-			new DBConn({
+		it("should correctly lowercase a username.", function() {
+			return new DBConn({
 				database: {
 					uri: "sqlite://charonauth/",
 					options: { "storage": ":memory:" }
@@ -51,31 +41,22 @@ describe('DBConn', function() {
 			}).then(function(dbconn) {
 				return dbconn.addUser('Username', 'password123', 'example@example.com')
 			}).then(function(user) {
-				if (user.username !== 'username') {
-					done(new Error("User was not added as expected"));
-				} else {
-					done();
-				}
+				assert.equal(user.username, 'username');
 			});
 		});
 	});
 	describe('DBConn.findUser()', function() {
-		it("should correctly find a user in a case-insensitive manner.", function(done) {
-			var db;
-
-			new DBConn({
+		it("should correctly find a user in a case-insensitive manner.", function() {
+			return new DBConn({
 				database: {
 					uri: "sqlite://charonauth/",
 					options: { "storage": ":memory:" }
 				}
-			}).then(function(dbconn) {
-				db = dbconn;
-				return require('./fixture/single_user')(db.User);
-			}).then(function() {
+			}).then(function(db) {
+				return Promise.all([db, require('./fixture/single_user')(db.User)]);
+			}).spread(function(db, _) {
 				return db.findUser('Username');
-			}).then(function(user) {
-				done();
-			}).catch(done);
+			});
 		});
 	});
 	describe('DBConn.verifyUser()', function() {
@@ -155,32 +136,25 @@ describe('DBConn', function() {
 		});
 	});
 	describe('DBConn.newSession()', function() {
-		it("should correctly create a session.", function(done) {
-			var db;
-
-			new DBConn({
+		it("should correctly create a session.", function() {
+			return new DBConn({
 				database: {
 					uri: "sqlite://charonauth/",
 					options: { "storage": ":memory:" }
 				}
-			}).then(function(dbconn) {
-				db = dbconn;
-				return require('./fixture/single_user')(db.User);
-			}).then(function() {
-				return db.newSession('username');
-			}).then(function(user) {
+			}).then(function(db) {
+				return Promise.all([db, require('./fixture/single_user')(db.User)]);
+			}).spread(function(db, _) {
+				return Promise.all([db, db.newSession('username')]);
+			}).spread(function(db, user) {
 				return db.Session.findAndCountAll();
 			}).then(function(sessions) {
-				if (sessions.count !== 1) {
-					done(new Error("Session was not created"));
-				}
+				assert.equal(sessions.count, 1, "Session was not created");
+
 				return sessions.rows[0].getUser();
 			}).then(function(user) {
-				if (user === null) {
-					done(new Error("Session is not attached to User"));
-				}
-				done();
-			}).catch(done);
+				assert.notEqual(user, null, "Session is not attached to User");
+			});
 		});
 	});
 });
